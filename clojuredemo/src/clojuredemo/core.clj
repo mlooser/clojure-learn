9ce99f41cde6bfd5dd32c0cf6d891da7c2d07550
Marcin Lusa <mlooser@gmail.com>
2020-09-21 13:49:19 +0200

    init dev

---
+++ clojuredemo/src/clojuredemo/core.clj
@@ -0,0 +1,21 @@
+(ns clojuredemo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(defn -main
+  "I don't do a whole lot...yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn my-first
+  [[first]]
+  (str "first arg is " first))
+
+(defn sum-numbers
+  "returns sum of two given numbers"
+  [arg1 arg2]
+  (+ arg1 arg1))
