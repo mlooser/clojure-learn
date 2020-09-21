7d6659114467255762ca050877256ad9ff0bc32a
mlooser <mlooser@gmail.com>
2020-09-21 17:12:23 +0200

    sum-lists with tests, improve sum-numbers

--- clojuredemo/src/clojuredemo/sum.clj
+++ clojuredemo/src/clojuredemo/sum.clj
@@ -3,9 +3,12 @@
 (defn sum-numbers
   "returns sum of two given numbers"
   [arg1 arg2]
-  (+ arg1 arg2))
+  (let [safeArg1 ( or arg1 0)
+        safeArg2 ( or arg2 0)]
+    (+ safeArg1 safeArg2))
+  )
 
-(defn sum-lists
+(defn sum-list-elements
   "returns sum of elements on given list"
   [list]
   (loop [sum 0
@@ -14,5 +17,20 @@
       sum
       (let [[first & rest] remaining]
         (recur (sum-numbers sum first) rest)))
+    ))
+
+(defn sum-lists
+  "returns list containing sum of suitable elements"
+  [list1 list2]
+  (loop [remaining1 list1
+         remaining2 list2
+         result []]
+      (if (and (empty? remaining1) (empty? remaining2))
+        result
+        ( let [[ first1 & rest1] remaining1
+               [first2 & rest2] remaining2]
+            (recur rest1 rest2 (conj result (sum-numbers first1 first2)))
+          )
+        )
     )
   )
--- clojuredemo/test/clojuredemo/sum_test.clj
+++ clojuredemo/test/clojuredemo/sum_test.clj
@@ -3,7 +3,18 @@
             [clojuredemo.sum :refer :all]))
 
 (deftest sum-numbers-test
-  (is ( = 5 ( sum-numbers 2 3))))
+  (is ( = 5 ( sum-numbers 2 3)))
+  (is ( = 2 ( sum-numbers 2 nil)))
+  (is ( = 3 ( sum-numbers nil 3)))
+  (is ( = 0 ( sum-numbers nil nil)))
+  )
 
-(deftest sum-list-test
-  (is ( = 10 ( sum-lists [1 2 3 4]))))
+(deftest sum-list-elements-test
+  (is ( = 10 ( sum-list-elements [1 2 3 4]))))
+
+(deftest sum-lists-test
+  (is ( = [3 5] ( sum-lists [1 2] [2 3])))
+  (is ( = [2 3] ( sum-lists [] [2 3])))
+  (is ( = [1 2] ( sum-lists [1 2] [])))
+  (is ( = [3 5 1] ( sum-lists [1 2] [2 3 1])))
+  )
