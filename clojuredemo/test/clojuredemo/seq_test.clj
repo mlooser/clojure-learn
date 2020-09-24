20a7010a5790699ed03d41d6cc236a707234b85b
mlooser <mlooser@gmail.com>
2020-09-24 09:50:33 +0200

    map function tests

---
+++ clojuredemo/test/clojuredemo/seq_test.clj
@@ -0,0 +1,24 @@
+(ns clojuredemo.seq-test
+  (:require [clojure.test :refer :all]))
+
+
+(deftest map-test
+  (is (= '(2 3 4) (map inc [1 2 3])))
+  (is (= '(3 5 4) (map + [1 2 3] [2 3 1])))
+  (is (= '("aA" "bB" "cC") (map str ["a" "b" "c"] ["A" "B" "C"])))
+  (is (= '({:first "fv1" :second "sv1"} {:first "fv2" :second "sv2"})
+         (map
+           (fn [f s] {:first f :second s})
+           ["fv1" "fv2"]
+           ["sv1" "sv2"])))
+  )
+
+(def sum #(reduce + %))
+(def avg #( / (sum %) (count %)))
+(defn stats
+  "returns sum, avg and count of given vector"
+  [numbers]
+  (map #(% numbers) [sum avg count]))
+
+(deftest stats-test
+  (is (= '(6 2 3) (stats [1 2 3]))))
