aeb0087bc83cf572707606db923afad3a00ab394
mlooser <mlooser@gmail.com>
2020-10-22 15:14:07 +0200

    tests of concat

--- clojuredemo/test/clojuredemo/misc_test.clj
+++ clojuredemo/test/clojuredemo/misc_test.clj
@@ -2,5 +2,9 @@
     (:require [clojure.test :refer :all]))
 
 (deftest assoc-test
-  (is (= {:a 1 :b 2} (assoc {:a 1} :b 2)))
-  )
+  (is (= {:a 1 :b 2} (assoc {:a 1} :b 2))))
+
+(deftest concat-test
+  (is
+   (= '(1 2 3 4)
+      (concat [1 2] [3 4]))))
