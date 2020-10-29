454263952882f7f5d686141f3ff345e0e330dff3
mlooser <mlooser@gmail.com>
2020-10-29 13:54:58 +0100

    [clojurdemo] assoc and get test

--- clojuredemo/test/clojuredemo/multimethod_test.clj
+++ clojuredemo/test/clojuredemo/multimethod_test.clj
@@ -11,6 +11,11 @@
            [params]
            (str (:val params) " in b"))
 
+(defmethod my-multi nil
+           [params]
+           (str (:val params) " in nil"))
+
 (deftest my-multi-test
   (is (= "A in a" (my-multi {:type :a :val "A"})))
-  (is (= "B in b" (my-multi {:type :b :val "B"}))))
+  (is (= "B in b" (my-multi {:type :b :val "B"})))
+  (is (= "N in nil" (my-multi {:val "N"}))))
